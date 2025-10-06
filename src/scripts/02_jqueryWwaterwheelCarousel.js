20d55efa8c8551592fc79764453981006199ab71
Кирилл Шустаев <kirill.shustaev@yandex.ru>
2025-10-06 16:28:08 +0300

    all

--- src/scripts/02_jqueryWwaterwheelCarousel.js
+++ src/scripts/02_jqueryWwaterwheelCarousel.js
@@ -40,7 +40,7 @@
         items:                  [],
         calculations:           [],
         carouselRotationsLeft:  0,
-        currentlyMoving:        false,
+        currentlyMoving:        true,
         itemsAnimating:         0,
         currentSpeed:           options.speed,
         intervalTimer:          null,
@@ -643,12 +643,12 @@
   $.fn.waterwheelCarousel.defaults = {
     // number tweeks to change apperance
     startingItem:               1,   // item to place in the center of the carousel. Set to 0 for auto
-    separation:                 175, // distance between items in carousel
+    separation:                 300, // distance between items in carousel
     separationMultiplier:       0.6, // multipled by separation distance to increase/decrease distance for each additional item
     horizonOffset:              0,   // offset each item from the "horizon" by this amount (causes arching)
     horizonOffsetMultiplier:    1,   // multipled by horizon offset to increase/decrease offset for each additional item
     sizeMultiplier:             0.7, // determines how drastically the size of each item changes
-    opacityMultiplier:          0.8, // determines how drastically the opacity of each item changes
+    opacityMultiplier:          1, // determines how drastically the opacity of each item changes
     horizon:                    0,   // how "far in" the horizontal/vertical horizon should be set from the container wall. 0 for auto
     flankingItems:              3,   // the number of items visible on either side of the center
 
@@ -663,7 +663,7 @@
     autoPlay:                   0,                 // indicate the speed in milliseconds to wait before autorotating. 0 to turn off. Can be negative
     orientation:                'horizontal',      // indicate if the carousel should be 'horizontal' or 'vertical'
     activeClassName:            'carousel-center', // the name of the class given to the current item in the center
-    keyboardNav:                false,             // set to true to move the carousel with the arrow keys
+    keyboardNav:                true,             // set to true to move the carousel with the arrow keys
     keyboardNavOverride:        true,              // set to true to override the normal functionality of the arrow keys (prevents scrolling)
     imageNav:                   true,              // clicking a non-center image will rotate that image to the center
 
